e90cae06a7044335e805ecd0485b90a81334f86b
Felipe Santos <felipesantossf@outlook.com>
2025-06-24 21:25:51 -0300

    Add configuration + database + .net8.0 + swagger

---
+++ Configuration/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.User;
+
+namespace Configuration.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+		public DbSet<User> Users { get; set; }
+	}
+}
--- Controllers/Program.cs
+++ Controllers/Program.cs
@@ -1,6 +1,17 @@
+using Configuration.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
-app.Run();
+builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("Test"));
