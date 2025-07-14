41ba11d6819ca375289244e445f48b9c8329fe26
Felipe Santos <felipesantossf@outlook.com>
2025-07-14 20:08:01 -0300

    add login dto

--- Dtos/Request/User/UserCreateRequest.cs
+++ Dtos/Request/User/UserCreateRequest.cs
@@ -5,8 +5,8 @@ namespace Dtos.Request.User
 	[ExcludeFromCodeCoverage]
 	public class UserCreateRequest
 	{
-		public required string Name { get; set; }
-		public required string Email { get; set; }
-		public required string Password { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public string Password { get; set; } = string.Empty;
 	}
 }
