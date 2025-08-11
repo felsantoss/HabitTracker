ca751906e8e1f110f42b18b8155d5b2a3867b90e
Felipe Santos <felipesantossf@outlook.com>
2025-08-11 20:07:06 -0300

    init authentication controller

---
+++ Controllers/Controllers/AuthenticationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+	[ApiController]
+	[Route("v1/api/authentication")]
+	[Produces("application/json")]
+	public class AuthenticationController
+	{
+	}
+}
