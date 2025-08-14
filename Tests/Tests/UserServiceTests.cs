8f639f5118faee953269085dff6053e7f84f50d3
Felipe Santos <felipesantossf@outlook.com>
2025-08-14 20:34:19 -0300

    init tests

---
+++ Tests/Tests/UserServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using Repositories.Interfaces;
+using Services.Services;
+
+
+namespace Tests.Tests
+{
+	public class UserServiceTests
+	{
+		private Mock<IUserRepository> _userRepositoryMock;
+
+		public UserServiceTests()
+		{
+			_userRepositoryMock = new Mock<IUserRepository>();
+
+			_userRepositoryMock.Setup(x => x.ExistsUserByEmailAsync(It.IsAny<string>()))
+							   .ReturnsAsync(false);
+
+			var service = new UserService(_userRepositoryMock.Object);
+		}
+	}
+}
