963ac53a0174db1f8b742550f745365de6c66de8
Felipe Santos <felipesantossf@outlook.com>
2025-08-14 21:28:01 -0300

    Add test create user

--- Tests/Tests/UserServiceTests.cs
+++ Tests/Tests/UserServiceTests.cs
@@ -1,6 +1,9 @@
-﻿using Moq;
+﻿using Dtos.Request.User;
+using Models.User;
+using Moq;
 using Repositories.Interfaces;
 using Services.Services;
+using System.Threading.Tasks;
 
 
 namespace Tests.Tests
@@ -15,8 +18,29 @@ namespace Tests.Tests
 
 			_userRepositoryMock.Setup(x => x.ExistsUserByEmailAsync(It.IsAny<string>()))
 							   .ReturnsAsync(false);
+		}
 
+		
+
+		[Fact(DisplayName = "Should Create When Email Not Exists")]
+		public async Task Should_Create_User_When_Email_Not_Exists() 
+		{
+			// Act
 			var service = new UserService(_userRepositoryMock.Object);
+
+			var request = new UserCreateRequest
+			{
+				Name = "name",
+				Email = "email@mail.com",
+				Password = "123456"
+			};
+
+			// Arrange
+			var result = await service.Create(request);
+
+			// Assert
+			Assert.Equal("name", request.Name);
+			_userRepositoryMock.Verify(x => x.Add(It.IsAny<User>()), Times.Once);
 		}
 	}
 }
